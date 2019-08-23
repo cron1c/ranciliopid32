79cb54af21d3a67e2c3e92a23bba39d3ecead0ea
cron1c <49157278+cron1c@users.noreply.github.com>
2019-08-23 19:32:24 +0200

    Add files via upload

---
+++ display.h
@@ -0,0 +1,21 @@
+// Screen dimensions
+#define SCREEN_WIDTH  128
+#define SCREEN_HEIGHT 128 // Change this to 96 for 1.27" OLED.
+
+// You can use any (4 or) 5 pins 
+#define SCLK_PIN 14
+#define MOSI_PIN 13
+#define CS_PIN   15
+#define DC_PIN   25
+#define RST_PIN  26
+
+
+// Color definitions
+#define  BLACK           0x0000
+#define BLUE            0x001F
+#define RED             0xF800
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0  
+#define WHITE           0xFFFF
---
+++ scale.h
@@ -0,0 +1,135 @@
+//-------------------------------------------------------------------------------------
+// HX711_ADC.h
+// Arduino master library for HX711 24-Bit Analog-to-Digital Converter for Weigh Scales
+// Olav Kallhovd sept2017
+// Tested with      : HX711 asian module on channel A and YZC-133 3kg load cell
+// Tested with MCU  : Arduino Nano, ESP8266
+//-------------------------------------------------------------------------------------
+// This is an example sketch on how to use this library
+// Settling time (number of samples) and data filtering can be adjusted in the config.h file
+
+#include <EEPROM.h>
+#include "userConfig.h"
+
+//HX711 constructor (dout pin, sck pin):
+
+int eepromAdress = 0;
+#include <HX711_ADC.h>
+HX711_ADC LoadCell(SCALE1,0);
+HX711_ADC LoadCell2(SCALE2,4);
+long tim;
+long tim2;
+
+void initScale() {
+  float calValue; // calibration value
+  calValue = 182.76;  //369.80; // uncomment this if you want to set this value in the sketch
+  //#if defined(ESP8266)
+  //EEPROM.begin(512); // uncomment this if you use ESP8266 and want to fetch this value from eeprom
+  //#endif
+  //EEPROM.get(eepromAdress, calValue); // uncomment this if you want to fetch this value from eeprom
+  LoadCell.begin();
+  long stabilisingtime = 2000; // tare preciscion can be improved by adding a few seconds of stabilising time
+  //LoadCell.start(stabilisingtime);
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Tare timeout, check MCU>HX711 wiring and pin designations");
+  }
+  else {
+    LoadCell.setCalFactor(calValue); // set calibration factor (float)
+    Serial.println("Startup + tare is complete");
+  }
+  while (!LoadCell.update());
+  Serial.print("Calibration factor: ");
+  Serial.println(LoadCell.getCalFactor());  
+  Serial.print("HX711 measured conversion time ms: ");
+  Serial.println(LoadCell.getConversionTime());
+  Serial.print("HX711 measured sampling rate HZ: ");
+  Serial.println(LoadCell.getSPS());
+  Serial.print("HX711 measured settlingtime ms: ");
+  Serial.println(LoadCell.getSettlingTime());
+  Serial.println("Note that the settling time may increase significantly if you use delay() in your sketch!");
+  if (LoadCell.getSPS() < 7) {
+    Serial.println("!!Sampling rate is lower than specification, check MCU>HX711 wiring and pin designations");
+  }
+  else if (LoadCell.getSPS() > 100) {
+    Serial.println("!!Sampling rate is higher than specification, check MCU>HX711 wiring and pin designations");
+  }
+}
+
+void initScale2() {
+  float calValue2; // calibration value
+  calValue2 = 202.01;  //369.80; // uncomment this if you want to set this value in the sketch
+  //#if defined(ESP8266)
+  //EEPROM.begin(512); // uncomment this if you use ESP8266 and want to fetch this value from eeprom
+  //#endif
+  //EEPROM.get(eepromAdress, calValue); // uncomment this if you want to fetch this value from eeprom
+  LoadCell2.begin();
+  long stabilisingtime = 2000; // tare preciscion can be improved by adding a few seconds of stabilising time
+  //LoadCell.start(stabilisingtime);
+  if (LoadCell2.getTareTimeoutFlag()) {
+    Serial.println("Tare timeout, check MCU>HX711 wiring and pin designations");
+  }
+  else {
+    LoadCell2.setCalFactor(calValue2); // set calibration factor (float)
+    Serial.println("Startup + tare is complete");
+  }
+  while (!LoadCell2.update());
+  Serial.print("Calibration factor: ");
+  Serial.println(LoadCell2.getCalFactor());  
+  Serial.print("HX711 measured conversion time ms: ");
+  Serial.println(LoadCell2.getConversionTime());
+  Serial.print("HX711 measured sampling rate HZ: ");
+  Serial.println(LoadCell2.getSPS());
+  Serial.print("HX711 measured settlingtime ms: ");
+  Serial.println(LoadCell2.getSettlingTime());
+  Serial.println("Note that the settling time may increase significantly if you use delay() in your sketch!");
+  if (LoadCell2.getSPS() < 7) {
+    Serial.println("!!Sampling rate is lower than specification, check MCU>HX711 wiring and pin designations");
+  }
+  else if (LoadCell2.getSPS() > 100) {
+    Serial.println("!!Sampling rate is higher than specification, check MCU>HX711 wiring and pin designations");
+  }
+}
+int getWeight() {
+  //update() should be called at least as often as HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS
+  //use of delay in sketch will reduce effective sample rate (be carefull with delay() in the loop)
+  //float sum = 0;
+  //int c;
+  float weight = 0;
+  //for (c = 0; c< 2; c++) {
+     LoadCell.update();
+  //get smoothed value from data set + current calibration factor
+  if (millis() > tim + 250) {
+    float i = LoadCell.getData();
+//    sum = sum + i;
+    Serial.print("Load_cell output val: ");
+    Serial.println(i);
+    weight = i;
+    tim = millis();
+  }
+  //float average = sum/2;
+  //return(average);
+  //}
+  return(weight);
+}
+int getWeight2() {
+  //update() should be called at least as often as HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS
+  //use of delay in sketch will reduce effective sample rate (be carefull with delay() in the loop)
+  //float sum = 0;
+  //int c;
+  float weight2 = 0;
+  //for (c = 0; c< 2; c++) {
+     LoadCell2.update();
+  //get smoothed value from data set + current calibration factor
+  if (millis() > tim2 + 250) {
+    float i = LoadCell2.getData();
+//    sum = sum + i;
+    Serial.print("Load_cell output val: ");
+    Serial.println(i);
+    weight2 = i;
+    tim2 = millis();
+  }
+  //float average = sum/2;
+  //return(average);
+  //}
+  return(weight2);
+}
